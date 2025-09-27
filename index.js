8969688eea188c5d3bb99e5764dca4c662e92b49
elkettani <youssef.elkettani@stud.th-luebeck.de>
2025-09-28 00:47:17 +0200

    Answer

--- index.js
+++ index.js
@@ -13,13 +13,37 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+app.get('/api/:date?',  (req, res) => {
+  let dateTime = req.params.date;
+  let date;
+
+  if (!dateTime) {
+    date = new Date();
+  }
+  else if (!isNaN(dateTime)) {
+    date = new Date(parseInt(dateTime));
+  }
+  else {
+    date =  new Date(dateTime)
+  }
+
+  if (date.toString() === "Invalid Date") return res.json({error: "Invalid Date"});
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+
+
+})
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
@@ -27,6 +51,6 @@ app.get("/api/hello", function (req, res) {
 
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3001, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
